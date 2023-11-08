42b0d7476396ffdec2c1be955d77f30791be39bb
Caio Adriano <caiobhadriano@gmail.com>
2023-11-08 19:31:45 -0300

    Inicializando o projeto e criando o primeiro component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import InputContainer from './components/InputContainer'
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Teste Projeto</h1>
+        <InputContainer></InputContainer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/InputContainer.jsx
@@ -0,0 +1,9 @@
+function InputContainer() {
+  return (
+    <div className="container-input">
+      <input type="text" placeholder="Digite seu cep..." />
+    </div>
+  );
+}
+
+export default InputContainer;
